d25c4d0d9748f0eb927ddc688c990d4f311dbe83
git.Ball <georgiballabanov@gmail.com>
2022-02-03 12:41:58 +0200

    Adding new mongoURL.

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 require('./Car')
-const connectionString = 'mongodb://localhost:27017/carbicle';
+// 127.0.0.1
+const connectionString = 'mongodb://127.0.0.1/carbicle';
+// const connectionString = 'mongodb://localhost:27017/carbicle';
                               //localhost:27017/
 async function init() {
   try {
